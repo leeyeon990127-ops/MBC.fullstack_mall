a214609bb48155c3232e1378b4a796a9ba380c7c
yeonsoo <leeyeon990127@gmail.com>
2026-07-03 17:46:24 +0900

    p140

---
+++ mallapi/src/test/java/org/zerock/mallapi/service/TodoServiceTests.java
@@ -0,0 +1,48 @@
+package org.zerock.mallapi.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.dto.PageRequestDTO;
+import org.zerock.mallapi.dto.PageResponseDTO;
+import org.zerock.mallapi.dto.TodoDTO;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@Log4j2
+public class TodoServiceTests {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister() {
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("")
+                .writer("")
+                .dueDate(LocalDate.of(2026,7,3))
+                .build();
+        Long tno = todoService.register(todoDTO);
+        log.info("TNO: " + tno);
+    }
+
+    @Test
+    public void testGet() {
+        Long tno = 100L;
+        TodoDTO todoDTO = todoService.get(tno);
+        log.info(todoDTO);
+    }
+
+    @Test
+    public void testList() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(2)
+                .size(10)
+                .build();
+
+        PageResponseDTO<TodoDTO> response = todoService.list(pageRequestDTO);
+        log.info(response);
+    }
+}
